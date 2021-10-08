a6c6ec8239c27d236ae898140771f6b5b5e798ca
ghaerdi <ghaerdi@outlook.com>
2021-10-08 15:12:19 -0400

    test: toCollection function

--- test/unit/collection.test.ts
+++ test/unit/collection.test.ts
@@ -1,28 +1,43 @@
 import { Collection } from "../../src/utils/collection";
+import { toCollection } from "../../src/utils/toCollection";
 
-describe("Collection", () => {
-	const collection = new Collection();
+describe("Collection class", () => {
+  const collection = new Collection();
 
-	it("check if inherits from array", () => {
-		expect(Array.isArray(collection)).toBe(true);
-	})
+  it("check if inherits from array", () => {
+    expect(Array.isArray(collection)).toBe(true);
+  });
 
-	it("should return an array of numbers", () => {
-		const collection = new Collection(1, 2, 3);
-		expect(collection).toEqual([1, 2, 3]);
-	})
+  it("should return an array of numbers", () => {
+    const collection = new Collection(1, 2, 3);
+    expect(collection).toEqual([1, 2, 3]);
+  });
 
-	it("check if swap method exist", () => {
-		expect(collection.swap).toBeDefined();
-	})
+  it("check if swap method exist", () => {
+    expect(collection.swap).toBeDefined();
+  });
 
-	it("swap two element", () => {
-		const collection = new Collection(1, 2, 3);
-		collection.swap(0, 1);
-		expect(collection).toEqual([2, 1, 3]);
-	})
+  it("swap two element", () => {
+    const collection = new Collection(1, 2, 3);
+    collection.swap(0, 1);
+    expect(collection).toEqual([2, 1, 3]);
+  });
 
-	it("check if shuffle method exist (uses shuffle function already)", () => {
-		expect(collection.shuffle).toBeDefined();
-	})
-})
+  it("check if shuffle method exist (uses shuffle function already)", () => {
+    expect(collection.shuffle).toBeDefined();
+  });
+});
+
+describe("toCollection function", () => {
+  it("should return a collection of bars", () => {
+    const numbers = [1, 2, 3];
+    const collection = toCollection(numbers);
+    expect(collection).toEqual(
+      [
+        { value: 1, color: "default" },
+        { value: 2, color: "default" },
+        { value: 3, color: "default" },
+      ],
+    );
+  });
+});
